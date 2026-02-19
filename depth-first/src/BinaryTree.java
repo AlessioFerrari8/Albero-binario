0a1396c6e2b15bf8fa5a4ff2d9533f93e2d668c3
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-19 11:42:37 +0100

    Feat: add class Node and binary tree class

---
+++ depth-first/src/BinaryTree.java
@@ -0,0 +1,11 @@
+public class BinaryTree {
+    private Node root; // head
+
+    public BinaryTree() {
+        this.root = null;
+    }
+
+    public BinaryTree(Node n) {
+        this.root = n;
+    }
+}
