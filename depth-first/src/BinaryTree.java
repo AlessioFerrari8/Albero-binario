cf73bf062a2e39a1d1e5500e6ccf863fef9417b5
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-02-19 12:00:06 +0100

    Feat: add pre-order method

--- depth-first/src/BinaryTree.java
+++ depth-first/src/BinaryTree.java
@@ -8,4 +8,27 @@ public class BinaryTree {
     public BinaryTree(Node n) {
         this.root = n;
     }
+    
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    // depth first
+
+    // in order
+    
+    /**
+     * I nodi genitori sono visitati prima dei figli
+     * @param n
+     */
+    private void preOrder(Node n) {
+        // exit clause
+        if (n == null) return;
+
+        System.out.print(n.getData());
+        preOrder(n.getLeft());
+        preOrder(n.getRight());
+    }
+
+    // post order
 }
